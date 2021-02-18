c7511d48b1c807392d3508aae58899193c2bf952
4uad <dmamadrid@gmail.com>
2021-02-18 19:18:44 +0100

    Updates

---
+++ precache-manifest.459a4afcf7829640ee81d8da1762e4cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ac618e6eba0f6caef3c6aac015852d",
+    "url": "/pactometro/index.html"
+  },
+  {
+    "revision": "d77aa836a70e9c37eb1e",
+    "url": "/pactometro/static/css/main.9e9b37e2.chunk.css"
+  },
+  {
+    "revision": "c96fc374185b7061f449",
+    "url": "/pactometro/static/js/2.0643c813.chunk.js"
+  },
+  {
+    "revision": "d77aa836a70e9c37eb1e",
+    "url": "/pactometro/static/js/main.4a7d1802.chunk.js"
+  },
+  {
+    "revision": "6aeaffb73e6956baacce",
+    "url": "/pactometro/static/js/runtime-main.b6dc5cc3.js"
+  },
+  {
+    "revision": "02b82122e2f769dce0d1574b2736e8a8",
+    "url": "/pactometro/static/media/eaj.02b82122.png"
+  }
+]);
